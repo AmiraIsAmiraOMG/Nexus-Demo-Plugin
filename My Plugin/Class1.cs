a1ce6d990b425410b1ea1a69e2e17ca0af355fb3
Wild Mastery Entertainment <167897093+AmiraIsAmiraOMG@users.noreply.github.com>
2024-12-22 20:25:22 +0000

    First Commit
    This was made at the time of NEXUS UTILITY BETA PRE RELEASE 1

---
+++ My Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using PluginSystem;
+
+namespace MyPlugin
+{
+    public class NotepadPlugin : IPlugin
+    {
+        public void Execute()
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+    }
+}
